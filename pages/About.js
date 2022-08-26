b1c5106cf4eab27681528ea2a85239298e84c9de
Ruzell11 <johnruzell123@gmail.com>
2022-08-26 18:27:25 +0800

    error

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const About = () => {
+export default function About(){
   return (
     <div name='About' className='lg:h-[85vh] flex items-center bg-black lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'>
       <div className='container mx-auto h-full'>
